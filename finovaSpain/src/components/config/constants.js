21dc1875e76f0385ce8339e0133939e8eb18c7f3
David <dkovel7@gmail.com>
2025-11-20 19:24:03 +0000

    updated config

--- finovaSpain/src/components/config/constants.js
+++ finovaSpain/src/components/config/constants.js
@@ -1,5 +1,5 @@
-export const CONFIG_API_BASE_URL ='http://localhost:8080';  
+export const CONFIG_API_BASE_URL ='https://finovaes.com';  
 
-// https://finovauz.com
+// https://finovaes.com
 // 'http://45.14.246.159:8080'; 
 // 'http://localhost:8080';  
