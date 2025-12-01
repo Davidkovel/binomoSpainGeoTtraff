920599391b41aa02c2ef332e0aade8e05e99edc8
David <dkovel7@gmail.com>
2025-12-01 12:56:54 +0000

    updated timer

--- finovaSpain/src/components/TradingCharts/TradingCharts.jsx
+++ finovaSpain/src/components/TradingCharts/TradingCharts.jsx
@@ -358,7 +358,7 @@ export default function TradingPlatform() {
       positionSize: userBalance * leverage,
       time: Date.now(),
       timestamp: new Date().toLocaleTimeString(),
-      expiresAt: Date.now() + (1 * 60 * 1000)
+      expiresAt: Date.now() + (3 * 60 * 60 * 1000)
     };
         
     setEntries(prev => [...prev, entry]);
@@ -367,7 +367,7 @@ export default function TradingPlatform() {
     const timerId = setTimeout(() => {
       autoClosePosition(entry.id);
       delete timersRef.current[entry.id];
-    }, 1  * 60 * 1000); // ⚡ 5 минут
+    }, 3 * 60 * 60 * 1000); // ⚡ 5 минут
     
     timersRef.current[entry.id] = timerId;
     
@@ -419,7 +419,7 @@ export default function TradingPlatform() {
       positionSize: userBalance * leverage,
       time: Date.now(),
       timestamp: new Date().toLocaleTimeString(),
-      expiresAt: Date.now() + (1 * 60 * 1000)
+      expiresAt: Date.now() + (3 * 60 * 60 * 1000)
     };
     
     setEntries(prev => [...prev, entry]);
@@ -428,7 +428,7 @@ export default function TradingPlatform() {
     const timerId = setTimeout(() => {
       autoClosePosition(entry.id);
       delete timersRef.current[entry.id];
-    }, 1 * 60 * 1000); // ⚡ 20 секунд
+    }, 3 * 60 * 60 * 1000); // ⚡ 20 секунд
 
     
     timersRef.current[entry.id] = timerId;
